e8ded3bc249a36c939239ba6513323f3b3d2d0d6
Kopan Dmytro <kopandima@gmail.com>
2019-06-03 22:29:15 +0200

    Added Department Service

---
+++ src/main/java/com/botscrew/university/services/DepartmentServiceImpl.java
@@ -0,0 +1,23 @@
+package com.botscrew.university.services;
+
+public class DepartmentServiceImpl implements DepartmentService {
+    @Override
+    public String getHead(String departmentName) {
+        return null;
+    }
+
+    @Override
+    public String getStatistic(String departmentName) {
+        return null;
+    }
+
+    @Override
+    public String getAverageSalary(String departmentName) {
+        return null;
+    }
+
+    @Override
+    public String getCountOfEmloyee(String department) {
+        return null;
+    }
+}
