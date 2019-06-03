db9fda7ecdcbdac1708becaa249286c21a8fc6fa
Kopan Dmytro <kopandima@gmail.com>
2019-06-03 22:00:31 +0200

    Added CrudRepository for Lector and Department

---
+++ src/main/java/com/botscrew/university/repositories/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.botscrew.university.repositories;
+
+import com.botscrew.university.models.Department;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends CrudRepository<Department,Long> {
+}
