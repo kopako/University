284f3805283ba66788824e5c015c03c788613177
Kopan Dmytro <kopandima@gmail.com>
2019-06-04 12:25:17 +0200

    docs(): Usage added If no argument provided

--- src/main/java/com/botscrew/university/UniversityApplication.java
+++ src/main/java/com/botscrew/university/UniversityApplication.java
@@ -27,7 +27,24 @@ public class UniversityApplication implements CommandLineRunner {
         if (args.length > 0) {
             mainService.recognizeRequestAndDelegate(args);
         } else {
-            System.out.println("No arguments found");
+            System.out.println("No arguments found\n" +
+                    "Usage:\n\n" +
+                    "Who is head of department {department_name}\n" +
+                    "Answer:\n" +
+                    "Head of {department_name} department is {head_of_department_name}\n" +
+                    "Show {department_name} statistic\n" +
+                    "Answer:\n" +
+                    "assistants - {assistants_count}. associate professors - {associate_professors_count} professors -{professors_count}\n" +
+                    "Show the average salary for department {department_name}\n" +
+                    "Answer:\n" +
+                    "The average salary of {department_name} is {average_salary}\n" +
+                    "Show count of employee for {department_name}\n" +
+                    "Answer:\n" +
+                    "{employee_count}\n" +
+                    "Global search by {template}\n" +
+                    "Example: Global search by van\n" +
+                    "Answer:\n" +
+                    "Ivan Petrenko, Petro Ivanov");
         }
     }
 }
